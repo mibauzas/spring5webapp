b1a61b3d9ac4b099a1a2583f319db0a98f4c5037
Miquel Bauza Soriano <mibauzas@gmail.com>
2020-01-21 21:26:20 +0100

    Adds book controller and view

--- spring5webapp/src/main/java/com/mibauzas/spring5webapp/model/Publisher.java
+++ spring5webapp/src/main/java/com/mibauzas/spring5webapp/model/Publisher.java
@@ -17,8 +17,11 @@ public class Publisher {
 	private String name;
 	private String address;
 	
+	public Publisher() {
+		
+	}
+	
 	public Publisher(String name, String address) {
-		super();
 		this.name = name;
 		this.address = address;
 	}
